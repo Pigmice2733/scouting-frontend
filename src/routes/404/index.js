e8897c7e77a9d2f2e2de0e1991bc426f74dd5bac
GreenBeard <alexcoffin1999@gmail.com>
2017-11-07 17:16:17 -0800

    Simplified CSS and HTML Tree

--- src/routes/404/index.js
+++ src/routes/404/index.js
@@ -1,13 +1,9 @@
 import { h } from 'preact'
-import style from './style'
 
 export default ({ eventId }) => (
-  <div class={style.home}>
+  <div>
     <h1>ERROR 404</h1>
     <h3>The requested file could not be found.</h3>
-    <br />
-    <div style="text-align: center;">
-        <video width="400" autoplay loop type="video/mp4" src="/assets/videos/burning_robot_404.mp4" />
-    </div>
+    <video autoplay loop type="video/mp4" src="/assets/videos/burning_robot_404.mp4" />
   </div>
 )

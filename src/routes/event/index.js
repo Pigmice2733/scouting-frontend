793ffc8f063fd0ace5c2e304a5cef252da9f0760
Caleb Eby <caleb.eby01@gmail.com>
2017-11-08 21:09:38 -0800

    Add basic event page with the given data

--- src/routes/event/index.js
+++ src/routes/event/index.js
@@ -1,9 +1,20 @@
 import { h } from 'preact'
+import wrap from '../../wrap'
+import { getEvent } from '../../api'
 
-const Event = ({ eventId }) => (
-  <div class="event">
-    <h1>Hi</h1>
-  </div>
+const Event = wrap(
+  ({ eventId, data = {} }) => {
+    const event = (data.event && data.event.event) || {}
+    const date = event.date && new Date(event.date)
+    return (
+      <div class="event">
+        <h1>{event.name || `Event ${eventId}`}</h1>
+        {date && <p>{date.toLocaleDateString()}</p>}
+        <pre>{JSON.stringify(data)}</pre>
+      </div>
+    )
+  },
+  ({ eventId }) => ({ event: getEvent(eventId) })
 )
 
 export default Event

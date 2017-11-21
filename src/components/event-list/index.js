34cadf320d969bfa5289ab17ec5369baf7d1f3a8
GreenBeard <alexcoffin1999@gmail.com>
2017-11-20 17:32:24 -0800

    Add proper date sorting for main page

--- src/components/event-list/index.js
+++ src/components/event-list/index.js
@@ -14,6 +14,30 @@ class EventList extends Component {
   }
 
   render({ events }, { query }) {
+    var presentFutureEvents = [];
+    var pastEvents = [];
+    const twoDaysAgo = Date.now() - (2 * 24 * 60 * 60 * 1000);
+    for (var e = 0; e < events.length; ++e) {
+      try {
+        events[e]['timestamp'] = (new Date(events[e]['date'])).getTime();
+        if (events[e]['timestamp'] >= twoDaysAgo) {
+          presentFutureEvents.push(events[e]);
+        } else {
+          pastEvents.push(events[e]);
+        }
+      } catch (err) {
+        console.log("Error event date invalid -" + JSON.stringify(events[e]) + " - ", err);
+      }
+      var date = new Date(events[e]['date']);
+      events[e]['name'] += " - " + (date.getMonth() + 0) + "/" + date.getDate() + "/" + date.getFullYear();
+    }
+    presentFutureEvents.sort(function(a, b) {
+      return a['timestamp'] - b['timestamp'];
+    });
+    pastEvents.sort(function(a, b) {
+      return b['timestamp'] - a['timestamp'];
+    });
+    const catEvents = presentFutureEvents.concat(pastEvents);
     return (
       <div class={style['event-list']}>
         <TextInput
@@ -23,7 +47,7 @@ class EventList extends Component {
           value={query}
         />
         <List>
-          {events
+          {catEvents
             .filter(e => e.name.toLowerCase().includes(query.toLowerCase()))
             .map(e => (
               <li key={e.key}>

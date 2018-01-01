00f123fbf13a338fad4aff901e556e7e52a1225c
Caleb Eby <caleb.eby01@gmail.com>
2018-01-01 14:58:16 -0800

    Remove unneccesary variable in utils

--- src/utils.tsx
+++ src/utils.tsx
@@ -27,18 +27,16 @@ const formatMatchId = (matchId: string): string => {
   return id
 }
 
-const twoDaysAgo = Date.now() - 2 * 24 * 60 * 60 * 1000
+const today = Number(new Date())
 
-const sortEvents = (events: FRCEvent[]) => {
-  const today = Number(new Date())
-  return events
+const sortEvents = (events: FRCEvent[]) =>
+  events
     .map(e => {
       e.parsedDate = new Date(e.date)
       e.distanceFromToday = Math.abs(Number(e.parsedDate) - today)
       return e
     })
     .sort((a, b) => (a.distanceFromToday > b.distanceFromToday ? 1 : -1))
-}
 
 const parseMatchKey = (name: string) => {
   const [, eventKey, matchKey] = name.match(/([^_]*)_(.*)/)

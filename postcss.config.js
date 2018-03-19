77147e08027b542c8173024408fecefcfa2347ec
Caleb Eby <caleb.eby01@gmail.com>
2018-03-18 21:27:39 -0700

    Fix postcss config for home page header

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ const vars = require('postcss-css-variables')
 module.exports = {
   parser: 'sugarss',
   plugins: {
+    'postcss-media-minmax': {},
     'postcss-nesting': {},
     'postcss-color-gray': {},
     'postcss-css-variables': {},

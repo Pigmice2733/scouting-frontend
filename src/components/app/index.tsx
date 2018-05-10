67d69f6b763a9dfa66b262c0c628e0c7f8e7dd3c
Caleb Eby <caleb.eby01@gmail.com>
2018-05-09 20:37:38 -0700

    Add back event and match pages

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -20,6 +20,8 @@ const App = () => (
   <div id={style.app}>
     <Router>
       <Route path="/" component={Home} />
+      <Route path="/events/:eventId" component={Event} />
+      <Route path="/events/:eventId/:matchId" component={Match} />
     </Router>
   </div>
 )
@@ -30,10 +32,8 @@ export default App
       <Route path="/leaderboard" component={Leaderboard} />
       <Route path="/login" component={Login} />
       <Route path="/admin" component={Admin} />
-      <Route path="/events/:eventId" component={Event} />
       <Route path="/events/:eventId/analysis" component={EventAnalysis} />
       <Route path="/events/:eventId/team/:team" component={TeamAnalysis} />
-      <Route path="/events/:eventId/:matchId" component={Match} />
       <Route path="/events/:eventId/:matchId/print" component={Print} />
       <Route
         path="/events/:eventId/:matchId/alliance/:color"
